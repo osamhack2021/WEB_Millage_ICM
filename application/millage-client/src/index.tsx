b564e026bb57ea7f76d281064511464afe256e5d
Soogeun Park <bwmelon97@naver.com>
2021-09-21 14:20:53 +0000

    chore: Modify code style

--- application/millage-client/src/index.tsx
+++ application/millage-client/src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './pages/App';
-import reducer from "@modules";
+import reducer from '@modules';
 
 const store = createStore(
-  reducer,
-  composeWithDevTools( /* applyMiddleware(...) */ )
-)
+    reducer,
+    composeWithDevTools( /* applyMiddleware(...) */ ),
+);
 
 ReactDOM.render(
     <React.StrictMode>
--- application/millage-client/src/modules/index.ts
+++ application/millage-client/src/modules/index.ts
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
-    /* Reducer List */
-})
+  /* Reducer List */
+});
 
 export default rootReducer;
 
