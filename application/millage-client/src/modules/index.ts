94476d96e72800731795de10828d548d8f0855be
Soogeun Park <bwmelon97@naver.com>
2021-09-21 13:59:47 +0000

    new: #37 Complete initial redux setting
    Resolve #37

--- application/millage-client/src/modules/index.ts
+++ application/millage-client/src/modules/index.ts
@@ -1,1 +1,9 @@
-export {}
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    /* Reducer List */
+})
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>
