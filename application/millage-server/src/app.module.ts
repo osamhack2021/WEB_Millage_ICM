e59eec768f0e7a5e7a7090f89692d5962ca22c5b
EunChan Park <pec9399@naver.com>
2021-09-28 13:50:44 +0000

    new: Add board entity and board list routing

--- application/millage-server/src/app.module.ts
+++ application/millage-server/src/app.module.ts
@@ -4,12 +4,14 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {EventsModule} from './events/events.module';
 import {UserModule} from './user/user.module';
+import {BoardModule} from './board/board.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     EventsModule,
     UserModule,
+    BoardModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- application/millage-server/src/user/user.module.ts
+++ application/millage-server/src/user/user.module.ts
@@ -4,7 +4,8 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {UserEntity} from './user.entity';
 import {UserService} from './user.service';
 import {UnitEntity} from '../unit/unit.entity';
-import { UserRoleEntity } from '../user_role/user_role.entity';
+import {UserRoleEntity} from '../user_role/user_role.entity';
+
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, UnitEntity, UserRoleEntity])],
   providers: [UserService],
