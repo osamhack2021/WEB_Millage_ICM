de2f958f55aed2ca8dfee893a4cc51a23e6d8d3c
hig4342 <46237722+hig4342@users.noreply.github.com>
2021-09-28 03:35:24 +0000

    improve: Change Ui library antd to mui

--- application/millage-client/src/pages/App.tsx
+++ application/millage-client/src/pages/App.tsx
@@ -7,6 +7,7 @@ import Signup from '@pages/Signup';
 import Main from '@pages/Main';
 import CreateBoardPage from '@pages/boards/CreateBoard';
 import CreatePostPage from '@pages/boards/CreatePost';
+import Schedule from '@pages/Schedule';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/register" component={Signup} />
         <Route path='/create-board' component={CreateBoardPage} />
         <Route path='/create' component={CreatePostPage} />
+        <Route path='/schedule' component={Schedule} />
       </Switch>
       <Footer/>
     </>
