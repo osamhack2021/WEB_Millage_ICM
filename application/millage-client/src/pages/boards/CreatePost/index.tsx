b1602b735c106ffd83f6b012dd9bda48ae406c9e
Soogeun Park <bwmelon97@naver.com>
2021-09-29 06:24:19 +0000

    refactor: Refactor types of Board

--- application/millage-client/src/pages/boards/CreatePost/index.tsx
+++ application/millage-client/src/pages/boards/CreatePost/index.tsx
@@ -10,23 +10,11 @@ import {
   TextareaAutosize,
 } from '@mui/material';
 import {NORMAL, POLL, RECRUIT} from '@constants';
-import {PollInput} from '@modules/board/types';
+import {PollInputs, PostInputs, PostType} from '@modules/board/types';
 import {CancelOutlined} from '@mui/icons-material';
 import {SubmitHandler, useForm} from 'react-hook-form';
 
-type PostType =
-  | typeof NORMAL
-  | typeof POLL
-  | typeof RECRUIT
-
 let newPollID = 1;
-
-type PostInputs = {
-  title: string;
-  content: string;
-  pollList?: PollInput[];
-  rCount?: number;
-}
 
 function CreatePostPage() {
   const {curBoard, boardList, getBoardList} = useBoard();
@@ -44,35 +32,35 @@ function CreatePostPage() {
   const [postType, setPostType] = useState<PostType>(NORMAL);
 
   // submit 전에 content가 ''인 poll 제거하는 로직 필요
-  const [pollList, setPollList] = useState<PollInput[]>([{
-    createId: newPollID,
+  const [pollInputs, setPollList] = useState<PollInputs[]>([{
+    index: newPollID,
     content: '',
   }]);
 
   // Poll 입력 이벤트 헨들러
   const onPollInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     // Target Poll Update
-    const targetPollIdx = pollList.findIndex((p) =>
-      p.createId === +e.target.id);
-    const updatedPollList: PollInput[] = [
-      ...pollList.slice(0, targetPollIdx),
+    const targetPollIdx = pollInputs.findIndex((p) =>
+      p.index === +e.target.id);
+    const updatedPollList: PollInputs[] = [
+      ...pollInputs.slice(0, targetPollIdx),
       {
-        createId: +e.target.id,
+        index: +e.target.id,
         content: e.target.value,
       },
-      ...pollList.slice(targetPollIdx + 1),
+      ...pollInputs.slice(targetPollIdx + 1),
     ];
 
     // 마지막 Poll이면 새로운 Poll 생성
-    const isLastInput = !(pollList.find((p) =>
-      p.createId === (+e.target.id) + 1));
+    const isLastInput = !(pollInputs.find((p) =>
+      p.index === (+e.target.id) + 1));
     if (isLastInput) {
       newPollID++;
       setPollList([
         ...updatedPollList,
         {
           content: '',
-          createId: newPollID,
+          index: newPollID,
         },
       ]);
     } else {
@@ -84,21 +72,21 @@ function CreatePostPage() {
   const onPollDelete = (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     const inputId =
       e.currentTarget.parentElement?.parentElement?.lastElementChild?.id;
-    if (!inputId || pollList.length === 1) {
+    if (!inputId || pollInputs.length === 1) {
       return;
     }
-    const targetPollIdx = pollList.findIndex((p) => p.createId === +inputId);
+    const targetPollIdx = pollInputs.findIndex((p) => p.index === +inputId);
     if (targetPollIdx === undefined) {
       return;
     }
     setPollList([
-      ...pollList.slice(0, targetPollIdx),
-      ...pollList.slice(targetPollIdx + 1),
+      ...pollInputs.slice(0, targetPollIdx),
+      ...pollInputs.slice(targetPollIdx + 1),
     ]);
   };
 
   const {register, handleSubmit} = useForm<PostInputs>({
-    defaultValues: {rCount: 0},
+    defaultValues: {recruitTotal: 0},
   });
   const onSubmit: SubmitHandler<PostInputs> = (data) => {
     if (!selectedBoard) {
@@ -115,13 +103,14 @@ function CreatePostPage() {
     }
 
     const dataWillSend: PostInputs = {
+      postType,
       title: data.title,
       content: data.content,
     };
     if (postType === POLL) {
-      dataWillSend.pollList = pollList.filter((p) => p.content !== '');
+      dataWillSend.pollInputs = pollInputs.filter((p) => p.content !== '');
     } else if (postType === RECRUIT) {
-      dataWillSend.rCount = data.rCount;
+      dataWillSend.recruitTotal = data.recruitTotal;
     }
     console.log(dataWillSend);
   };
@@ -215,9 +204,9 @@ function CreatePostPage() {
           <div className='w-full'>
             <h3 className='text-xl mt-4 mb-2' >설문지 만들기</h3>
             <div className='w-full'>
-              { pollList.map((p) => (
+              { pollInputs.map((p) => (
                 <div
-                  key={p.createId}
+                  key={p.index}
                   className='relative ring-1 ring-gray-500'
                 >
                   <div
@@ -232,7 +221,7 @@ function CreatePostPage() {
                     />
                   </div>
                   <input
-                    id={p.createId.toString()}
+                    id={p.index.toString()}
                     value={p.content}
                     onChange={onPollInputChange}
                     className='w-full focus:outline-none p-4'
@@ -248,7 +237,7 @@ function CreatePostPage() {
           <div className='flex my-6 items-center' >
             <h3 className='text-xl mr-4' >모집 인원</h3>
             <input
-              {...register('rCount')}
+              {...register('recruitTotal')}
               className='p-2 ring-1 ring-gray-500 focus:outline-none'
               type='number'
               min={0}
