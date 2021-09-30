74dcfe6b63e7baafc058727da1b3aed9d6e8ebb1
EunChan Park <pec9399@naver.com>
2021-09-30 23:17:35 +0900

    new: Add global navigation bar

--- application/millage-client/src/components/Header/index.tsx
+++ application/millage-client/src/components/Header/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import SendIcon from '@mui/icons-material/Send';
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import {Link as RouterLink} from 'react-router-dom';
 import './header.css';
 
 function Header() {
+  const user = useSelector((state: any) => state.user);
   return (
-    <header>
+    <header className="header">
+      <div className="logo">
+        <img src='img/logo.png'/>
+        <div className="unitNameContainer">
+          <div className="millage">
+            밀리지
+          </div>
+          <div className="unitName">
+            {user.session.unit.name}
+          </div>
+        </div>
+      </div>
+      <div className="navigation">
+        <RouterLink className="enabled" to='/'>게시판</RouterLink>
+        <RouterLink to='/schedule'>캘린더</RouterLink>
+        <RouterLink to='/schedule'>시설예약</RouterLink>
+        <RouterLink to='/'>전군게시판</RouterLink>
+      </div>
+      <div className="buttons">
+        <IconButton component={RouterLink} to='/dm'>
+          <SendIcon />
+        </IconButton>
+        <IconButton component={RouterLink} to='/user'>
+          <PersonIcon />
+        </IconButton>
+      </div>
     </header>
   );
 }
--- application/millage-client/src/pages/Main/index.tsx
+++ application/millage-client/src/pages/Main/index.tsx
@@ -2,13 +2,14 @@ import React, {useEffect} from 'react';
 import {RouteComponentProps} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
 import {Route, Switch} from 'react-router-dom';
+import Header from '@components/Header';
 import {checkSessionAsync} from '@modules/User/actions';
 import CreateBoardPage from '@pages/boards/CreateBoard';
 import CreatePostPage from '@pages/boards/CreatePost';
 import Schedule from '@pages/Schedule';
 import Intro from '@pages/Intro';
-import Header from '@components/Header';
 import MainPage from './MainPage';
+import DM from '@pages/DM';
 
 const Main = ({history}: RouteComponentProps) => {
   const dispatch = useDispatch();
@@ -21,12 +22,13 @@ const Main = ({history}: RouteComponentProps) => {
   if (session.role.id > 0) {
     return (
       <>
-        <Header />
+        <Header/>
         <Switch>
           <Route exact path='/' component={MainPage} />
           <Route path='/create-board' component={CreateBoardPage} />
           <Route path='/create' component={CreatePostPage} />
           <Route path='/schedule' component={Schedule} />
+          <Route path='/dm' component={DM} />
         </Switch>
       </>
     );
