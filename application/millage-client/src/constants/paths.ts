bd0153fb212fd6337e6088c74d9f0e54460a36e1
Soogeun Park <bwmelon97@naver.com>
2021-10-01 11:20:08 +0000

    chore: #130 Change Paths as constants
    pathname이 literal string으로 작성될 경우 오타 등으로 오류가 발생할 수 있고,
    어떤 페이지를 라우팅하는 지 명확하지 못한 경우가 있습니다.
    따라서 이를 constant로 관리하도록 변경하였습니다.
    
    Resolves #130
    See Also #129

---
+++ application/millage-client/src/constants/paths.ts
@@ -0,0 +1,11 @@
+export const ROOT_PATH = '/';
+
+export const SIGNIN_PATH = '/login';
+export const REGISTER_PATH = '/register';
+export const SIGNUP_PATH = '/register/user';
+
+export const SCHEDULE_PATH = '/schedule';
+export const DM_PATH = '/dm';
+export const CREATE_POST_PATH = '/create';
+export const CREATE_BOARD_PATH = '/create-board';
+export const BOARD_VIEW_PATH = '/board/:boardId';
