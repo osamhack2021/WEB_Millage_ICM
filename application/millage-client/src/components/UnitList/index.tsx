587a10cd799e471c1a2200001d03f0362aea0787
EunChan Park <pec9399@naver.com>
2021-10-01 20:00:47 +0900

    refactor: Export page path to constants

--- application/millage-client/src/components/UnitList/index.tsx
+++ application/millage-client/src/components/UnitList/index.tsx
@@ -5,6 +5,7 @@ import {useDispatch, useSelector} from 'react-redux';
 import CSS from 'csstype';
 import './unit.css';
 import {useHistory, useLocation} from 'react-router-dom';
+import {REGISTER_PATH, ROOT_PATH} from '@constants';
 
 export default function UnitList() {
   const location = useLocation();
@@ -13,7 +14,7 @@ export default function UnitList() {
   let containerStyle: CSS.Properties = {
     height: '100%',
   };
-  if (page == '/register') {
+  if (page === REGISTER_PATH) {
     containerStyle = {
       margin: '50px auto 0 auto',
       width: '480px',
@@ -62,14 +63,14 @@ export default function UnitList() {
 
   const renderUnitList = () => {
     return unitList.map((u: UnitObject) => {
-      if (page=='/') {
+      if (page===ROOT_PATH) {
         return (
           <div className="unit">
             <span className="name">{u.name}</span>
             <span className="count">{u.count}명</span>
           </div>
         );
-      } else if (page=='/register') {
+      } else if (page===REGISTER_PATH) {
         return (
           <a className="link" onClick={()=>{
             setKeyword(u.name);
@@ -84,7 +85,7 @@ export default function UnitList() {
   };
 
   let button;
-  if (page=='/register') {
+  if (page===REGISTER_PATH) {
     button = (
       <div className="nextButton">
         <button onClick={()=>{
