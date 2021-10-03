b4d7506316836a52212a591f3b4003b22ce237ac
EunChan Park <pec9399@naver.com>
2021-10-03 10:47:52 +0000

    refactor: Add base url for backend requests

--- application/millage-client/src/modules/User/api.tsx
+++ application/millage-client/src/modules/User/api.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export async function createUserApi(data: UserSubmitData) : Promise<UserState> {
   try {
-    const user = await axios.post('/user/register', data);
+    const user = await axios.post('/api/user/register', data);
     return user.data;
   } catch (err: any) {
     return {result: 'error', message: err};
@@ -12,7 +12,7 @@ export async function createUserApi(data: UserSubmitData) : Promise<UserState> {
 
 export async function loginApi(data: UserLoginData) : Promise<UserState> {
   try {
-    const user = await axios.post('/user/login', data);
+    const user = await axios.post('/api/user/login', data);
     return user.data;
   } catch (err: any) {
     return {result: 'error'};
@@ -21,7 +21,7 @@ export async function loginApi(data: UserLoginData) : Promise<UserState> {
 
 export async function sessionApi() : Promise<UserState> {
   try {
-    const session = await axios.get('/user/session');
+    const session = await axios.get('/api/user/session');
     return session.data;
   } catch (err: any) {
     return {result: 'error'};
