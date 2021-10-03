173191653bb52ebc92bfa560fe0d2a056ffb506d
EunChan Park <pec9399@naver.com>
2021-10-03 17:38:04 +0900

    fix: Remove trailing space

--- application/millage-client/src/hooks/usePrevious.ts
+++ application/millage-client/src/hooks/usePrevious.ts
@@ -4,7 +4,7 @@ import {useEffect, useRef} from 'react';
  * parameter로 넘겨받은 value를 ref의 current에 저장해서,\
  * useEffect 이후 render되기 이전에 update되기 이전의 값을\
  * 사용할 수 있도록 해주는 hook
- * @param {any} value 
+ * @param {any} value
  * @return {any}
  */
 function usePrevious(value: any): any {
