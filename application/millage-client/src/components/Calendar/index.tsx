f233cfce9a8fd31977b062e38816326e398c5bb4
Yellowinq <hig4342@naver.com>
2021-10-04 14:34:17 +0000

    improve: add edit,delete Button & Modal

--- application/millage-client/src/components/Calendar/index.tsx
+++ application/millage-client/src/components/Calendar/index.tsx
@@ -11,23 +11,43 @@ import './Calendar.css';
 
 const Calendar: React.FC = () => {
   const [visible, setVisible] = React.useState(false);
+  const [state, setState] = React.useState<'add' | 'edit' | 'delete'>('add');
   const [events, setEvents] = React.useState<Array<EventInput>>([{
     id: '1',
     title: 'hello',
-    start: new Date('2021-09-23 12:00'),
-    end: new Date('2021-09-24 12:00'),
+    start: new Date('2021-10-23 12:00'),
+    end: new Date('2021-10-24 12:00'),
+    content: 'test1',
   }, {
     id: '2',
     title: 'dang jik',
-    start: new Date('2021-09-28'),
+    start: new Date('2021-10-28'),
+    content: 'test2',
   }]);
 
   const handleOpen = () => setVisible(true);
   const handleClose = () => setVisible(false);
 
   const addEvents: CustomButtonInput = {
-    text: '추가하기',
+    text: '추가',
     click: () => {
+      setState('add');
+      handleOpen();
+    },
+  };
+
+  const editEvents: CustomButtonInput = {
+    text: '편집',
+    click: () => {
+      setState('edit');
+      handleOpen();
+    },
+  };
+
+  const deleteEvents: CustomButtonInput = {
+    text: '삭제',
+    click: () => {
+      setState('delete');
       handleOpen();
     },
   };
@@ -43,22 +63,26 @@ const Calendar: React.FC = () => {
         height={1000}
         headerToolbar={{
           start: 'prev,next today',
-          center: '',
-          end: 'addEvents dayGridMonth,timeGridWeek,timeGridDay',
+          center: 'addEvents,editEvents,deleteEvents',
+          end: 'dayGridMonth,timeGridWeek,timeGridDay',
         }}
         events={events}
         editable
         fixedWeekCount={false}
         customButtons={{
           addEvents,
+          editEvents,
+          deleteEvents,
         }}
         eventDrop={(({event}) => {
+          console.log(event.toJSON());
           const tmp: EventInput = {
             id: event.id,
+            groupId: event.groupId,
             title: event.title,
+            extendedProps: event.extendedProps,
             start: event.start ? event.start : undefined,
             end: event.end ? event.end : undefined,
-            url: event.url,
             color: event.textColor,
           };
           setEvents([tmp, ...events.filter((e) => e.id != tmp.id)]);
@@ -66,8 +90,8 @@ const Calendar: React.FC = () => {
       />
       <Dialog
         visible={visible}
-        handleOpen={handleOpen}
         handleClose={handleClose}
+        state={state}
       />
     </>
   );
