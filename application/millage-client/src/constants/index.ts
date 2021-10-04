87f447bb7c55d5a940bfca16521e92d1973c3c02
EunChan Park <pec9399@naver.com>
2021-10-04 04:47:59 +0000

    fix: Seperate socket server to port 3001

--- application/millage-client/src/constants/index.ts
+++ application/millage-client/src/constants/index.ts
@@ -8,4 +8,5 @@ export const NORMAL = 'NORMAL';
 export const POLL = 'POLL';
 export const RECRUIT = 'RECRUIT';
 
-export const SERVER = 'https://millage.ml/api';
+export const SERVER = 'https://millage.ml/api';    // 'http://localhost:3000/api'
+export const SOCKET_SERVER = 'https://millage.ml'; // 'http://localhost:3001' 
