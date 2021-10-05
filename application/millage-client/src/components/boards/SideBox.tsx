25c5d74183eed249c72c0b1c8bd29af6322276ab
Soogeun Park <bwmelon97@naver.com>
2021-10-05 11:01:26 +0000

    new: #184 Create BoardHeader & SideBox components

---
+++ application/millage-client/src/components/boards/SideBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SideBox() {
+  return (
+    <div className='
+      hidden lg:block w-72 ml-6
+      ring-1 ring-gray-500 min-h-full flex-col
+    '>
+      Side Component
+    </div>
+  );
+}
+
+export default SideBox;
