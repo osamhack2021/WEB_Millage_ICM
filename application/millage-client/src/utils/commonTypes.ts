303ca5bb3c51637764c0138a043dce7b483b6ae8
Soogeun Park <bwmelon97@naver.com>
2021-10-05 11:43:08 +0000

    new: Create ClassNameProps type
    리팩토링한 컴포넌트에게 ClassName을 추가로 부여할 수 있도록
    className을 props로 넘길 수 있게 해주는 PropsType

--- application/millage-client/src/utils/commonTypes.ts
+++ application/millage-client/src/utils/commonTypes.ts
@@ -7,3 +7,11 @@ export type PaginationResults<T> = {
     totalCounts: number;    // 총 데이터 수
     totalPages: number;     // 총 페이지 수
 };
+
+/**
+ * 리팩토링한 컴포넌트에게 ClassName을 추가로 부여할 수 있도록\
+ * className을 props로 넘길 수 있게 해주는 PropsType
+ */
+export type ClassNameProps = {
+    className?: string;
+};
