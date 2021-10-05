933ee6484f6de266e6a20fea5b442d2e256bccd1
EunChan Park <pec9399@naver.com>
2021-10-05 13:40:44 +0000

    fix: Fix Message page api requests

--- application/millage-client/src/modules/Unit/api.tsx
+++ application/millage-client/src/modules/Unit/api.tsx
@@ -5,7 +5,8 @@ import {SERVER} from '@constants';
 
 export async function getUnitListApi() : Promise<UnitState> {
   try {
-    const list = await axios.get(`${SERVER}/unit/list`);
+    const list = await axios.get(`${SERVER}/unit/list`,
+        {withCredentials: true});
     return list.data;
   } catch (err: any) {
     return {result: 'error', message: 'Unit list fetch failed'};
