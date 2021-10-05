0c5494f5a60bca47409a3261d06aa490b02f7901
Soogeun Park <bwmelon97@naver.com>
2021-10-05 11:01:26 +0000

    new: #184 Create BoardHeader & SideBox components

---
+++ application/millage-client/src/components/boards/BoardHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BoardHeader() {
+  return (
+    <div className='bg-gray-300 py-4'>
+      <div className='max-w-screen-xl mx-auto'>
+        Board Header
+      </div>
+    </div>
+  );
+}
+
+export default BoardHeader;
