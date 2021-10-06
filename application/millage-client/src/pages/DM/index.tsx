f3ad0344045086edb2c1c5d400f7b11714fcb4e6
EunChan Park <pec9399@naver.com>
2021-10-06 10:55:51 +0000

    improve: Enable socket communication between users

--- application/millage-client/src/pages/DM/index.tsx
+++ application/millage-client/src/pages/DM/index.tsx
@@ -6,38 +6,66 @@ import {SubmitHandler, useForm} from 'react-hook-form';
 import {useDispatch, useSelector} from 'react-redux';
 import {getMessageBoxListAsync, getMessagesAsync} from '@modules/DM/actions';
 
-interface MessageData {
+interface MessageInterface {
   message: string;
+}
+
+export interface MessageData{
+  id: number;
+  receiverId: number;
+  senderId: number;
+  senderName: string;
+  message: string;
+  time: string;
 }
 
 function DM() {
   const dispatch = useDispatch();
 
   const messageboxes = useSelector((state: any) => state.DM.messageboxes);
+  const user = useSelector((state: any) => state.user);
+  const session = user.session;
   const messages = useSelector((state: any) => state.DM.messages);
-  const {register, handleSubmit} = useForm<MessageData>();
+  const {register, handleSubmit} = useForm<MessageInterface>();
   const [socket] = useState(io(SOCKET_SERVER, {transports: ['websocket']}));
-
+  const [receiverId, setReceiverId] = useState(-1);
   const getMessage = (id: number) => {
-    console.log(id);
     dispatch(getMessagesAsync.request(id));
+    setReceiverId(id);
   };
+
+  socket.on('msgToClient', (data:MessageData) => {
+    console.log(data);
+  });
 
   useEffect(() => {
     dispatch(getMessageBoxListAsync.request());
-    socket.emit('events', {name: 'Nest'}, (data: any) => console.log(data));
+    socket.emit('subscribe',
+        {id: session.id}, (data: any) => console.log(data));
   }, [dispatch]);
 
   useEffect(() => {
-    console.log(messages);
+    // console.log(messages);
   }, [messages]);
 
   const renderMessageBoxes = () => {
     return messageboxes.map((mb: any) => {
       return (
-        <button onClick={()=>getMessage(mb.senderId)}
-          key={mb.id}>{mb.message}
-        </button>
+        <div>
+          <button onClick={()=>getMessage(mb.senderId)}
+            key={mb.id}>{mb.message}
+          </button>
+        </div>
+      );
+    });
+  };
+
+  const renderMessages = () => {
+    return messages.map((m: any) => {
+      return (
+        <div key={m.id}>
+          {m.message}
+        </div>
       );
     });
   };
@@ -46,13 +74,20 @@ function DM() {
     if (e) {
       e.preventDefault();
     }
-    console.log(data);
+    socket.emit('msgToServer', {
+      message: data.message,
+      senderId: session.id,
+      receiverId: receiverId,
+      anonymous: false,
+    });
   };
 
   return (
-    <div id="container">
+    <div id="MessageContainer">
       <div id="messageboxes">
-        <h2>메시지 함</h2>
+        <div className="title">
+          <h2>메시지 함</h2>
+        </div>
         <div className="items">
           {renderMessageBoxes()}
         </div>
@@ -61,6 +96,7 @@ function DM() {
         <div className="title"></div>
         <div className="items"></div>
         <form onSubmit={handleSubmit(onSubmit)}>
+          {renderMessages()}
           <input type="text" placeholder="메시지 입력"
             {...register('message', {})}
           />
