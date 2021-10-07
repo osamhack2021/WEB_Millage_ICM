c979a61c8eec350fc9142ae040a2280bcf51d50a
Soogeun Park <bwmelon97@naver.com>
2021-10-07 04:16:24 +0000

    refactor: Change name MainPage -> BoardMainPage
    Main/index.tsx 컴포넌트와 헷깔리지 않기 위해 이름을 변경하였습니다.

---
+++ application/millage-client/src/pages/boards/BoardMainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BoardMainPage() {
+  return (
+    <div>
+      <div className='bg-gray-800 h-40 w-full'>
+        공모전 포스터 이미지 들어갈 예정
+      </div>
+    </div>
+  );
+}
+
+
+export default BoardMainPage;
