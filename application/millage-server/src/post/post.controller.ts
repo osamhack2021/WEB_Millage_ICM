cb1329db95903fdec02f2a45318ec2d2e8ad4a06
Seongheum Choi <kingman330@gmail.com>
2021-10-07 01:18:49 +0000

    feat: Getting post by id

--- application/millage-server/src/post/post.controller.ts
+++ application/millage-server/src/post/post.controller.ts
@@ -1,8 +1,8 @@
-import {Post, Body, Controller} from '@nestjs/common';
+import {Get, Post, Body, Controller, Param} from '@nestjs/common';
 import {ApiTags, ApiBearerAuth} from '@nestjs/swagger';
 import {PostService} from './post.service';
 import {PostRO} from './post.interface';
-import {CreatePostDto} from './dto';
+import {CreatePostDto, GetPostParams} from './dto';
 import {Result} from '../common/common.interface';
 
 @ApiBearerAuth()
@@ -14,12 +14,31 @@ export class PostController {
   @Post('create')
   async register(@Body() postdata : CreatePostDto): Promise<PostRO> {
     try {
-      const postRO = await this.postService.create(postdata);
-      return postRO;
+      const savedPost = await this.postService.create(postdata);
+      return {
+        result: Result.SUCCESS,
+        post: savedPost,
+      };
     } catch (err) {
       return {
-        result: Result.FAIL,
-        message: err,
+        result: Result.ERROR,
+        message: err.message,
+      };
+    }
+  }
+
+  @Get(':id')
+  async get(@Param() params: GetPostParams): Promise<PostRO> {
+    try {
+      const selectedPost = await this.postService.get(params.id);
+      return {
+        result: Result.SUCCESS,
+        post: selectedPost,
+      }
+    } catch (err) {
+      return {
+        result: Result.ERROR,
+        message: err.message,
       };
     }
   }
