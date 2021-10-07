ad89fc319aa3fe54a665d3238d088d424d472254
Soogeun Park <bwmelon97@naver.com>
2021-10-07 13:52:46 +0000

    refactor: Refactor Components of BoardHeader

---
+++ application/millage-client/src/components/boards/BoardHeader/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BoardHeaderLoading() {
+  return (
+    <div>
+      loading...
+    </div>
+  );
+}
+
+export default BoardHeaderLoading;
--- application/millage-client/src/components/boards/BoardHeader/index.tsx
+++ application/millage-client/src/components/boards/BoardHeader/index.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect} from 'react';
 import {XLayout} from '@components/common';
 import {useBoard} from '@hooks/board';
-import {Link} from 'react-router-dom';
-import {BOARD_PATH} from '@constants';
+import BoardHeaderLoading from './Loading';
+import BoardLink from './BoardLink';
 
 function BoardHeader() {
   const {getBoardList, boardListState} = useBoard();
@@ -16,18 +16,16 @@ function BoardHeader() {
     <div className='bg-gray-300 py-4 flex justify-center items-center'>
       <XLayout>
         { loading || !data ?
-          <div>
-            loading...
-          </div> :
+
+          <BoardHeaderLoading /> :
 
           <div className='max-w-5xl flex flex-wrap px-2 justify-center'>
             {data.map( (board) => (
-              <Link
-                to={`${BOARD_PATH}/${board.id}`}
-                className='text-base text-gray-600 hover:underline mx-2'
-              >
-                {board.title}
-              </Link>
+              <BoardLink
+                id={board.id}
+                title={board.title}
+                key={board.id}
+              />
             ))}
           </div>
         }
