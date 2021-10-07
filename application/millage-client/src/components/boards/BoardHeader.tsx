b97b345dd70b9bd3ba7a5f5d25ef7f1ade7a7a2d
Soogeun Park <bwmelon97@naver.com>
2021-10-07 13:41:07 +0000

    feat: #171 Implement Board Link Component

--- application/millage-client/src/components/boards/BoardHeader.tsx
+++ application/millage-client/src/components/boards/BoardHeader.tsx
@@ -1,18 +1,36 @@
 import React, {useEffect} from 'react';
 import {XLayout} from '@components/common';
 import {useBoard} from '@hooks/board';
+import {Link} from 'react-router-dom';
+import {BOARD_PATH} from '@constants';
 
 function BoardHeader() {
-  const {getBoardList} = useBoard();
+  const {getBoardList, boardListState} = useBoard();
+  const {loading, data} = boardListState;
 
   useEffect(() => {
     getBoardList();
   }, []);
 
   return (
-    <div className='bg-gray-300 py-4'>
+    <div className='bg-gray-300 py-4 flex justify-center items-center'>
       <XLayout>
-        Board Header
+        { loading || !data ?
+          <div>
+            loading...
+          </div> :
+
+          <div className='max-w-5xl flex flex-wrap px-2 justify-center'>
+            {data.map( (board) => (
+              <Link
+                to={`${BOARD_PATH}/${board.id}`}
+                className='text-base text-gray-600 hover:underline mx-2'
+              >
+                {board.title}
+              </Link>
+            ))}
+          </div>
+        }
       </XLayout>
     </div>
   );
