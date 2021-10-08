b88b69dc106909d4af20ad8e17d1084c14ac973c
Soogeun Park <bwmelon97@naver.com>
2021-10-08 04:15:00 +0000

    feat: #198 Style Board Grid Component & Items

--- application/millage-client/src/pages/boards/BoardMainPage.tsx
+++ application/millage-client/src/pages/boards/BoardMainPage.tsx
@@ -6,6 +6,103 @@ function BoardMainPage() {
       <div className='bg-gray-800 h-40 w-full'>
         공모전 포스터 이미지 들어갈 예정
       </div>
+
+      <div
+        className={`
+          mt-4
+          grid grid-cols-1 sm:grid-cols-2 gap-2
+        `}
+      >
+        <div className='ring-1 ring-gray-300 flex flex-col' >
+          <div className='
+            px-3 py-4 ring-1 ring-gray-300
+            text-green text-lg font-bold
+          '>
+            Board Title
+          </div>
+
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+
+        </div>
+        <div className='ring-1 ring-gray-300 flex flex-col' >
+          <div className='
+            px-3 py-4 ring-1 ring-gray-300
+            text-green text-lg font-bold
+          '>
+            Board Title
+          </div>
+
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+
+        </div>
+        <div className='ring-1 ring-gray-300 flex flex-col' >
+          <div className='
+            px-3 py-4 ring-1 ring-gray-300
+            text-green text-lg font-bold
+          '>
+            Board Title
+          </div>
+
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+
+        </div>
+        <div className='ring-1 ring-gray-300 flex flex-col' >
+          <div className='
+            px-3 py-4 ring-1 ring-gray-300
+            text-green text-lg font-bold
+          '>
+            Board Title
+          </div>
+
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+          <div className='p-3 border-t border-gray-300'>
+            Board Item
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
