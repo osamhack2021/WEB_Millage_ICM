ecc7c6f50c4a7f7d0b032489a15586e24b0dba2f
Soogeun Park <bwmelon97@naver.com>
2021-10-08 08:04:58 +0000

    feat: change margin left of SideBox
    XLayout의 padding 값인 16px로 통일

--- application/millage-client/src/components/boards/SideBox.tsx
+++ application/millage-client/src/components/boards/SideBox.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SideBox() {
   return (
     <div className='
-      hidden lg:block w-72 ml-6
+      hidden lg:block w-72 ml-4
       ring-1 ring-gray-500 min-h-full flex-col
     '>
       Side Component
