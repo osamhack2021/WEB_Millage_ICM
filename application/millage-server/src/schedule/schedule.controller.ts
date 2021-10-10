4748ec7cb378f84c69f1873a330a78d7dd838303
Seongheum Choi <kingman330@gmail.com>
2021-10-10 06:20:01 +0000

    feat: Add getRecentScheduleForUnit in controller

--- application/millage-server/src/schedule/schedule.controller.ts
+++ application/millage-server/src/schedule/schedule.controller.ts
@@ -1,5 +1,7 @@
 import {Controller, Get, Param, Req} from '@nestjs/common';
 import {ApiTags, ApiBearerAuth} from '@nestjs/swagger';
+import {Request} from 'express';
+
 import {ResultObject, Result} from '../common/common.interface';
 
 @ApiBearerAuth()
@@ -8,22 +10,38 @@ import {ResultObject, Result} from '../common/common.interface';
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
-  @Get(':groupId')  // validation 필요
-  async getUserSchedule(
-    @Req() req: Request,
-    @Param('unitId') unitId: number
-  ): Promise<ResultObject> {
-    try{
+  @Get('/personal') // validation 필요
+  async getUserSchedule(@Req() req: Request): Promise<ResultObject> {
+    try {
       return {
         result: Result.SUCCESS,
-        schedule_list: await this.scheduleService.get(req.session.user.id, groupId);
+        schedules: await this.scheduleService.getUserSchedule(
+          req.session.user.id, 
+          req.session.user.unit.id,
+        ),
       };
     } catch (err) {
       return {
         result: Result.ERROR,
         message: err.message,
-      }
+      };
     }
   }
 
+  @Get('/recentUnit')
+  async getRecentScheduleForUnit(@Req() req: Request): Promise<ResultObject> {
+    try {
+      return {
+        result: Result.SUCCESS,
+        schedules: await this.scheduleService.getRecentScheduleForUnit(
+          req.session.user.unit.id,
+        ),
+      };
+    } catch (err) {
+      return {
+        result: Result.ERROR,
+        message: err.message,
+      };
+    }
+  }
 }
