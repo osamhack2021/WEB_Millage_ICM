4fcbe564a3ba8e07ea9d91351de481482e04b826
Soogeun Park <bwmelon97@naver.com>
2021-10-10 14:43:50 +0000

    chore: Restore API paths

--- application/millage-client/src/constants/APIs.ts
+++ application/millage-client/src/constants/APIs.ts
@@ -1,5 +1,5 @@
-export const SERVER         = '/api';    // 'http://localhost:3000/api'
-export const SOCKET_SERVER  = ''; // 'http://localhost:3001'
+export const SERVER         = 'https://millage.ml/api';    // 'http://localhost:3000/api'
+export const SOCKET_SERVER  = 'https://millage.ml/'; // 'http://localhost:3001'
 
 export const BOARD_API                      = `${SERVER}/board`;
 export const GET_BOARD_LIST_API             = `${BOARD_API}/list`;
