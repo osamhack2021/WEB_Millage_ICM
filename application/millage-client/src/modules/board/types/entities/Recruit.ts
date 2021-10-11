d62b8524f746fe924b9c1b9c2af5f6941cb56487
Soogeun Park <bwmelon97@naver.com>
2021-10-11 13:20:28 +0000

    fix: Synchronize type of Recruit

--- application/millage-client/src/modules/board/types/entities/Recruit.ts
+++ application/millage-client/src/modules/board/types/entities/Recruit.ts
@@ -1,6 +1,8 @@
+import {UserData} from '@modules/User/types';
+
 /* Types of Recruit */
 export type RecruitStatus = {
     status: 'progress' | 'completed'
     totalMember: number;
-    currentMember: number;
+    currentMember: UserData[];
 };
