61326d2733fca27f75a5af2beb70124c974f8372
Seongheum Choi <kingman330@gmail.com>
2021-10-12 00:53:50 +0000

    feat: Add create comment dto

---
+++ application/millage-server/src/post/comment/dto/create-comment.dto.ts
@@ -0,0 +1,11 @@
+import {IsNotEmpty} from 'class-validator';
+
+export class CreateCommentDto {
+  @IsNotEmpty()
+  content: string;
+
+  @IsNotEmpty()
+  postId: number;
+
+  parentCommentId?: number;
+}
