91feb82c8ce496bc7da7c1e19b120b80994b4508
Soogeun Park <bwmelon97@naver.com>
2021-10-12 14:18:40 +0000

    refactor: Refactor Components of BoardViewPage

---
+++ application/millage-client/src/components/boards/BoardView/index.ts
@@ -0,0 +1,2 @@
+export {default as BoardBoxTop} from './BoardBoxTop';
+export {default as PostListBox} from './PostListBox';
--- application/millage-client/src/components/boards/index.ts
+++ application/millage-client/src/components/boards/index.ts
@@ -1,6 +1,3 @@
-export {default as BoardBoxTop} from './BoardBoxTop';
-export {default as PostListBox} from './PostListBox';
-export {default as PostListItem} from './PostListItem';
 export {default as BoardHeader} from './BoardHeader';
 export {default as SideBox} from './SideBox';
 export {default as BoardTitle} from './BoardTitle';
