b99bed5e0700f3c30711c9ad7f4bba3ad374e3cd
Soogeun Park <bwmelon97@naver.com>
2021-10-12 14:06:19 +0000

    fix: Fix Auth value in BoardView CreatePost Button

--- application/millage-client/src/components/boards/BoardBoxTop.tsx
+++ application/millage-client/src/components/boards/BoardBoxTop.tsx
@@ -43,7 +43,7 @@ const BoardBoxTop: React.FC<Props> = ({auth}) => {
         </button>
       </div>
 
-      { auth === 0 &&
+      { auth === 1 &&
         <Link
           to='/create'
           className='px-4 py-2 ring-1 ring-gray-500'
