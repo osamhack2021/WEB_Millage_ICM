df446290cfa8348d9a1e632c7f264fdaae304539
Seongheum Choi <kingman330@gmail.com>
2021-10-13 10:31:22 +0000

    feat: Add getAllPlace method in service

---
+++ application/millage-server/src/place/place.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+
+import {PlaceEntity} from './.place.entity';
+
+@Injectable()
+export class PlaceService {
+  constructor(
+    @InjectRepository(PlaceEntity)
+    private readonly placeRepository: Repository<PlaceEntity>,
+  ) {}
+
+  async getAllPlace(unitId: number): Promise<PlaceEntity[]> {
+    return await this.placeRepository.find({where: {unitId: unitId}});
+    // reservations relation 필요
+  }
+}
