bcd833934f40d6b064d48f075d6cfd7173874483
Seongheum Choi <kingman330@gmail.com>
2021-10-13 13:42:54 +0000

    feat: Add reservation module

--- application/millage-server/src/place/place.module.ts
+++ application/millage-server/src/place/place.module.ts
@@ -1,15 +1,11 @@
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {PlaceEntity} from './place.entity';
-import {ReservationEntity} from './reservation/reservation.entity';
 import {PlaceController} from './place.controller';
 import {PlaceService} from './place.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-    PlaceEntity,
-    ReservationEntity,
-  ])],
+  imports: [TypeOrmModule.forFeature([PlaceEntity])],
   providers: [PlaceService],
   controllers: [PlaceController],
   exports: [PlaceService],
