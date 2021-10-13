240ad8172f81b3e31a3567741d052f3d1a65b3fc
Seongheum Choi <kingman330@gmail.com>
2021-10-13 11:11:27 +0000

    feat: Add reservation entity

--- application/millage-server/src/place/place.entity.ts
+++ application/millage-server/src/place/place.entity.ts
@@ -1,8 +1,8 @@
 import {UnitEntity} from '../unit/unit.entity';
 import {
-  Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne,
-  JoinTable, OneToOne, RelationId, ManyToMany, AfterLoad,
+  Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, JoinColumn,
 } from 'typeorm';
+import {ReservationEntity} from './reservation/reservation.entity';
 
 @Entity('place')
 export class PlaceEntity {
@@ -15,9 +15,15 @@ export class PlaceEntity {
   @Column()
   seats: number;
 
-  @ManyToOne(() => UnitEntity, (unit) => unit.places)
+  @ManyToOne(() => UnitEntity)
+  @JoinColumn({name: 'unitId', referencedColumnName: 'id'})
   unit: UnitEntity;
 
   @Column()
   unitId: number;
+
+  @OneToMany(() => ReservationEntity, (reservation) => reservation.place, {
+    onDelete: 'CASCADE',
+  })
+  reservations: ReservationEntity[];
 }
--- application/millage-server/src/place/place.service.ts
+++ application/millage-server/src/place/place.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 
-import {PlaceEntity} from './.place.entity';
+import {PlaceEntity} from './place.entity';
 
 @Injectable()
 export class PlaceService {
