9dbe2e3c519fd11106eec2caa2fbdeb769290ee1
Soogeun Park <bwmelon97@naver.com>
2021-10-13 14:16:19 +0000

    feat: Implement ImageBox of BoardMain

--- application/millage-client/src/components/boards/BoardMain/ImageBox/index.tsx
+++ application/millage-client/src/components/boards/BoardMain/ImageBox/index.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import ImageItem from './ImageItem';
+import Img1 from './images/Img1.svg';
+import Img2 from './images/Img2.svg';
+import Img3 from './images/Img3.svg';
+import Img4 from './images/Img4.svg';
 
 function ImageBox() {
   return (
-    <div className='
-      bg-red-200 overflow-x-scroll
-      h-40 min-w-full flex flex-nowrap
-    '>
-      공모전 포스터 이미지 들어갈 예정
+    <div className='overflow-x-auto mb-4'>
+      <div
+        style={{
+          minWidth: '800px',
+        }}
+        className='h-40 grid grid-cols-4 gap-6'
+      >
+        <ImageItem
+          imgPath={Img1}
+          title={'9월 대대 진급식'}
+        />
+        <ImageItem
+          imgPath={Img2}
+          title={'추석 맞이 차례'}
+        />
+        <ImageItem
+          imgPath={Img3}
+          title={'9월 삽겹살 데이'}
+        />
+        <ImageItem
+          imgPath={Img4}
+          title={'화랑 리그 개최'}
+        />
+      </div>
     </div>
   );
 }
