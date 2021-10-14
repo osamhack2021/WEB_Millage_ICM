a3235f221a073b21aec72a4263a0dfe7bf5dd99d
EunChan Park <pec9399@naver.com>
2021-10-14 05:29:44 +0000

    improve: Add manage page submenu

--- application/millage-client/src/pages/Manage/index.tsx
+++ application/millage-client/src/pages/Manage/index.tsx
@@ -1,12 +1,40 @@
 import AdminUsers from '@components/Admin/AdminUsers';
-
+import {setPageStateAction} from '@modules/Admin/actions';
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {XLayout} from '@components/common';
+import './manage.css';
 function Manage() {
+  const dispatch = useDispatch();
+  const adminState = useSelector((state: any) => state.admin);
+  const setPageState = (page: string) => {
+    dispatch(setPageStateAction(page));
+  };
+
   return (
-    <div id="AdminContainer">
-      <AdminUsers />
+    <div id="ManageContainer">
+      <XLayout className='navigation h-full flex justify-between items-center'>
+        <a className={adminState.page == 'users' ? 'enabled' : ''}
+          onClick={()=>setPageState('users')}>
+            사용자관리
+        </a>
+        <a className={adminState.page == 'boards' ? 'enabled' : ''}
+          onClick={()=>setPageState('boards')}>
+            게시판관리
+        </a>
+        <a className={adminState.page == 'posts' ? 'enabled' : ''}
+          onClick={()=>setPageState('posts')}>
+            게시글관리?
+        </a>
+        <a className={adminState.page == 'places' ? 'enabled' : ''}
+          onClick={()=>setPageState('places')}>
+            시설관리
+        </a>
+      </XLayout>
+      <div id="AdminContainer">
+        {adminState.page == 'users' ? <AdminUsers /> : ''}
+      </div>
     </div>
   );
 }
-
-
 export default Manage;
