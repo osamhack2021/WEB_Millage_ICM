f7818ae1b2d1b7f4009288b6c028647eca397b66
Soogeun Park <bwmelon97@naver.com>
2021-10-15 14:59:16 +0000

    feat: Move CreatePostPage into BoardRoutes

--- application/millage-client/src/pages/boards/BoardRoutes.tsx
+++ application/millage-client/src/pages/boards/BoardRoutes.tsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import {
   BOARD_PATH,
   BOARD_VIEW_PATH,
+  CREATE_POST_PATH,
   POST_VIEW_PATH,
   ROOT_PATH,
 } from '@constants';
@@ -11,6 +12,7 @@ import BoardViewPage from './BoardViewPage';
 import {BoardHeader, SideBox} from '@components/boards';
 import {XLayout} from '@components/common';
 import PostViewPage from './PostViewPage';
+import CreatePostPage from './CreatePostPage';
 
 function BoardRoutes() {
   return (
@@ -32,6 +34,7 @@ function BoardRoutes() {
             <Route exact path={BOARD_PATH} component={BoardMainPage} />
             <Route path={POST_VIEW_PATH} component={PostViewPage} />
             <Route path={BOARD_VIEW_PATH} component={BoardViewPage} />
+            <Route path={CREATE_POST_PATH} component={CreatePostPage} />
           </Switch>
         </div>
 
