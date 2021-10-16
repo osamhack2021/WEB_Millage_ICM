cc5d78fdbb3cdd2a63e7225c75e74b74affe8635
Soogeun Park <bwmelon97@naver.com>
2021-10-16 04:54:13 +0000

    refactor: Refactor InputTitle Component

---
+++ application/millage-client/src/components/boards/CreatePost/InputTitle.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const InputTitle: React.FC = ({children}) => {
+  return (
+    <h3 className='text-lg mr-6'>
+      {children}
+    </h3>
+  )
+};
+
+export default InputTitle;
