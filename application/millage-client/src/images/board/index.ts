b1ff46d04806378ada196b31c4e9f1cb407ca791
EunChan Park <pec9399@naver.com>
2021-10-16 15:00:06 +0000

    add: Like filled images

--- application/millage-client/src/images/board/index.ts
+++ application/millage-client/src/images/board/index.ts
@@ -9,6 +9,8 @@ export {default as ReplyIcon} from './sidebox/reply.svg';
 export {default as ReplyButton} from './ReplyButton.svg';
 export {default as Comment} from './Comment.svg';
 export {default as Like} from './Like.svg';
+export {default as LikeColorFilled} from './Like_Color_Filled.svg';
+export {default as LikeBlackFilled} from './Like_Black_Filled.svg';
 export {default as NormalPost} from './Normal_Post.svg';
 export {default as PollPost} from './Poll_Post.svg';
 export {default as RecruitPost} from './Recruit_Post.svg';
