6d4b86a58450f302e1cddbbd3c71735d1d34b0dc
Soogeun Park <bwmelon97@naver.com>
2021-10-16 05:50:26 +0000

    feat: Add width style in InputTitle

--- application/millage-client/src/components/boards/CreatePost/InputComponents/InputTitle.tsx
+++ application/millage-client/src/components/boards/CreatePost/InputComponents/InputTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const InputTitle: React.FC = ({children}) => {
   return (
-    <h3 className='mb-2 sm:mb-0 text-lg mr-6'>
+    <h3 className='w-28 mb-2 sm:mb-0 text-lg mr-6'>
       {children}
     </h3>
   );
