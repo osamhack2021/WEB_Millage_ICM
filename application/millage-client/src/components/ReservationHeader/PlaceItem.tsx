bfb2bd670f273eb725d1a5bcee9e4ae0b9de4e88
Yellowinq <hig4342@naver.com>
2021-10-18 13:11:45 +0000

    style: Change Reservation style

--- application/millage-client/src/components/ReservationHeader/PlaceItem.tsx
+++ application/millage-client/src/components/ReservationHeader/PlaceItem.tsx
@@ -2,14 +2,22 @@ import * as React from 'react';
 import type {Place} from '@modules/Reservation/types';
 import {Link} from 'react-router-dom';
 import {RESERVATION_PATH} from '@constants';
+import {Button} from '@mui/material';
 
 interface Props {
   place: Place;
+  onClick: React.MouseEventHandler<HTMLAnchorElement>
+  isSelected: boolean;
 }
-const PlaceItem: React.FC<Props> = ({place}) => {
+const PlaceItem: React.FC<Props> = ({place, onClick, isSelected}) => {
   return (
-    <Link to={`${RESERVATION_PATH}/${place.id}`}>
-      {place.name}
+    <Link
+      to={`${RESERVATION_PATH}/${place.id}`}
+      onClick={onClick}
+    >
+      <Button variant={isSelected ? 'contained' : 'outlined'}>
+        {place.name}
+      </Button>
     </Link>
   );
 };
--- application/millage-client/src/pages/Reservation/index.tsx
+++ application/millage-client/src/pages/Reservation/index.tsx
@@ -3,7 +3,6 @@ import {Route, Switch} from 'react-router-dom';
 import {default as Calendar} from '@components/ReservationCalendar';
 import {default as Header} from '@components/ReservationHeader';
 import {
-  RESERVATION_PATH,
   RESERVATION_VIEW_PATH,
 } from '@constants';
 import './reservation.css';
