9e220c7a39c54b9107fc10b68bb4d74af8b1ae34
Soogeun Park <bwmelon97@naver.com>
2021-10-18 03:52:43 +0000

    feat: Style RecruitBox in PostViewPage

--- application/millage-client/src/components/boards/PostView/RecruitBox.tsx
+++ application/millage-client/src/components/boards/PostView/RecruitBox.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import {useBoard} from '@hooks/board';
 import {RecruitStatus} from '@modules/board/types';
+import {FilledButton} from '@components/common';
 
 type Props = RecruitStatus & {
   postId: number;
 }
 
 const RecruitBox: React.FC<Props> = ({
-  status, totalMember, currentMember, isMember, postId,
+  isMember, postId,
 }) => {
   const {toggleRecruit} = useBoard();
   return (
-    <div>
-      {status}
-      {currentMember.length}
-      {totalMember}
-      <button onClick={() => toggleRecruit({postId})}>
-        {isMember ? '취소' : '참가'}
-      </button>
+    <div className='w-full flex flex-col items-center mb-8'>
+      <FilledButton
+        className='w-32'
+        onClick={() => toggleRecruit({postId})}
+      >
+        {isMember ? '참가취소' : '참가신청'}
+      </FilledButton>
     </div>
   );
 };
